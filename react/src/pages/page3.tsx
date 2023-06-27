53c063dfb3066ac3392adce3cbc13f12b2cf6e60
chris <chris@modugen.de>
2023-06-27 17:35:23 +0200

    Add form

--- react/src/pages/page3.tsx
+++ react/src/pages/page3.tsx
@@ -1,11 +1,46 @@
-import { MapControls, OrthographicCamera } from '@react-three/drei'
+import { Line, MapControls, OrthographicCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { ReactElement } from 'react'
 import { Euler } from 'three'
+import { Form, TextField } from '../components'
+import { number, object } from 'yup'
+import { Button, Divider, Stack } from '@mui/material'
+import usePage3Store from './page3/store'
 
 const Page3 = (): ReactElement => {
+  const walls_floorplan = usePage3Store((state) => state.walls)
+
+  const schema = object({
+    x_start: number().required().min(0).max(10).default(0),
+    y_start: number().required().min(0).max(10).default(0),
+  })
+
+  const onClickDeleteWall = () => {
+    alert('TODO: delete')
+  }
+
   return (
     <>
+      <Form onSubmit={() => alert('submitted')} validationSchema={schema}>
+        <Stack direction="column" spacing={2}>
+          <TextField name="x_start" placeholder="X Start" sx={{ width: 300 }} />
+
+          <TextField name="y_start" placeholder="Y Start" sx={{ width: 300 }} />
+
+          <Button type="submit" sx={{ width: 200 }}>
+            Wand hinzufügen
+          </Button>
+        </Stack>
+      </Form>
+
+      <Divider />
+
+      <Button sx={{ width: 200 }} onClick={onClickDeleteWall}>
+        Ausgewählte Wand löschen
+      </Button>
+
+      <Divider />
+
       <Canvas style={{ height: '100vH' }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
@@ -37,6 +72,18 @@ const Page3 = (): ReactElement => {
           // test
           screenSpacePanning={true}
         />
+
+        {/* SCENE CONTENT */}
+
+        {walls_floorplan.map((wall, index) => (
+          <Line
+            points={[wall.start, wall.end]} // Array of points, Array<Vector3 | Vector2 | [number, number, number] | [number, number] | number>
+            color="black" // Default
+            lineWidth={4} // In pixels (default)
+            segments // If true, renders a THREE.LineSegments2. Otherwise, renders a THREE.Line2
+            dashed={false} // Default
+          />
+        ))}
       </Canvas>
     </>
   )
---
+++ react/src/pages/page3/store.tsx
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import { walls_floorplan } from '../../data/task2'
+import { reject } from 'lodash-es'
+
+interface State {
+  walls: Wall[]
+}
+
+const usePage3Store = create<State>((set) => ({
+  walls: walls_floorplan,
+
+  addWall: (wall: Wall) =>
+    set((state: State) => ({ walls: [...state.walls, wall] })),
+
+  removeWall: (index: number) =>
+    set((state: State) => ({
+      walls: reject(state.walls, (_, i) => i === index),
+    })),
+}))
+
+export default usePage3Store
